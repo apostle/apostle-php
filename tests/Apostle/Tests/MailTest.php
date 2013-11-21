46b8bdeaf434930e32e6aeb2963c097baad9065a
Mal Curtis <mal@sitepoint.com>
2013-11-22 11:19:31 +1300

    Remove more syntax from tests

--- tests/Apostle/Tests/MailTest.php
+++ tests/Apostle/Tests/MailTest.php
@@ -9,24 +9,24 @@ class MailTest extends TestCase
 {
 	public function testConstructorSetsData()
 	{
-		$mail = new Mail("slug", [
+		$mail = new Mail("slug", array(
 			"email" => "user@example.org",
 			"name" => "Example User",
-			"headers" => ["headers"],
+			"headers" => array("headers"),
 			"from" => "from@example.org",
 			"layoutId" => "layout-slug",
 			"replyTo" => "reply@example.org",
 			"other" => "data",
 			"more" => "data"
-		]);
+		));
 
 		$this->assertEquals("user@example.org", $mail->email);
 		$this->assertEquals("Example User", $mail->name);
-		$this->assertEquals(["headers"], $mail->headers);
+		$this->assertEquals(array("headers"), $mail->headers);
 		$this->assertEquals("from@example.org", $mail->from);
 		$this->assertEquals("layout-slug", $mail->layoutId);
 		$this->assertEquals("reply@example.org", $mail->replyTo);
-		$this->assertEquals(["other" => "data", "more" => "data"], $mail->data);
+		$this->assertEquals(array("other" => "data", "more" => "data"), $mail->data);
 	}
 
 	public function testSetter()
@@ -35,22 +35,22 @@ class MailTest extends TestCase
 
 		$mail->potato = "hardy";
 
-		$this->assertEquals(["potato" => "hardy"], $mail->data);
+		$this->assertEquals(array("potato" => "hardy"), $mail->data);
 	}
 
 	public function testToArray()
 	{
-		$mail = new Mail("slug", [
+		$mail = new Mail("slug", array(
 			"email" => "user@example.org",
 			"from" => "from@example.org",
 			"random" => "data"
-		]);
+		));
 
-		$this->assertEquals([
+		$this->assertEquals(array(
 			"template_id" => "slug",
 			"from" => "from@example.org",
-			"data" => ["random" => "data"],
-		], $mail->toArray());
+			"data" => array("random" => "data"),
+		), $mail->toArray());
 	}
 
 	public function testDeliverSetsFailure()
@@ -63,7 +63,7 @@ class MailTest extends TestCase
 
 	public function testDeliverSuccess()
 	{
-		$mail = new Mail("slug", ["email" => "user@example.org"]);
+		$mail = new Mail("slug", array("email" => "user@example.org"));
 
 		$this->assertTrue($mail->deliver($failure));
 		$this->assertNull($failure);
